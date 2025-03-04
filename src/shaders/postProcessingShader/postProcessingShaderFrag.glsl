75a5929787ef3dcc9e5a6ee6312ecb2672059cba
also focks <etng@madison.k12.wi.us>
2025-03-04 14:22:06 -0600

    put skybox rendering in post processing pass

--- src/shaders/postProcessingShader/postProcessingShaderFrag.glsl
+++ src/shaders/postProcessingShader/postProcessingShaderFrag.glsl
@@ -3,6 +3,7 @@ out vec4 fragColor;
 in vec2 texCoord;
 
 uniform sampler2D ppBuffer;
+uniform sampler2D skybox;
 
 uniform float exposure;
 uniform float gamma;
@@ -10,11 +11,15 @@ uniform float gamma;
 
 void main() {
     vec4 c = texture(ppBuffer, texCoord).rgba;
-    vec3 hdrColor = c.rgb;
-
-    vec3 mapped = vec3(1.0) - exp(-hdrColor * exposure);
-    // gamma correction
-    mapped = pow(mapped, vec3(1.0 / gamma));
-
-    fragColor = vec4(mapped, 1.0);
+    if (c.a > 0) {
+        vec3 hdrColor = c.rgb;
+    
+        vec3 mapped = vec3(1.0) - exp(-hdrColor * exposure);
+        // gamma correction
+        mapped = pow(mapped, vec3(1.0 / gamma));
+    
+        fragColor = vec4(mapped, 1.0);
+    } else {
+        fragColor = vec4(texture(skybox, texCoord).rgb, 1);
+    }
 }
